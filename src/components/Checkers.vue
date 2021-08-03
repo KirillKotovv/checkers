6ddd00499820ac7eabdb6e2c33ac8b0aee5ecf01
bogdan.hlyan <es.bogdan.hlyan@gmail.com>
2021-08-03 15:07:21 +0300

    refactor

--- src/components/Checkers.vue
+++ src/components/Checkers.vue
@@ -1,95 +1,121 @@
 <template>
-    <div class="checkers">
-        <div class="checkers__title">шашки</div>
-        <div class="checkers__field">
-            <div class="checkers__row" v-for="(x,indexRow) in position_x" :key="indexRow">
-                <div class="checkers__cell" v-for="(y,index) in position_y" :key="index"
-                     :class="{'checkers__cell_black-color': ((indexRow + 1)%2 == 0 && (index+1) %2 !=0) || ((indexRow + 1)%2 != 0 && (index+1)% 2 ==0) }">
-                    <div v-if="(indexRow == 5 && (index)%2 == 0) || (indexRow == 6 && (index)%2 != 0) || (indexRow == 7 && (index)%2 == 0) ||
-                                    (indexRow == 0 && (index)%2 != 0) || (indexRow == 1 && (index)%2 == 0) || (indexRow == 2 && (index)%2 != 0)"
-                         class="checkers__checker" :class="{'checkers__checker_red-color':indexRow>=5}"></div>
-                </div>
-            </div>
-        </div>
+  <div class="checkers">
+    <div class="checkers__title">шашки</div>
+    <div>
+      <div v-for="(line, index) in board" :key="index">{{line}}</div>
     </div>
+    <br>
+    {{selectedCell}}
+    <br>
+    <div class="checkers__field" :key="boardKey">
+      <div class="checkers__row" v-for="(line, lineIndex) in board" :key="lineIndex + Math.random()">
+        <div class="checkers__cell"
+             :class="{'checkers__cell_black-color': getCellColor(lineIndex, cellIndex) }"
+             v-for="(cell, cellIndex) in line"
+             :key="cellIndex + Math.random()"
+             @click="handleClickCell(lineIndex, cellIndex, cell)">
+          <div v-if="cell !== 0" class="checkers__checker" :class="{'checkers__checker_red-color': cell === 2}"></div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Checkers",
-        data: () => {
-            return {
-                check: [0, 0],
-                blue_checkers_position: [{x: 8, y: 2}, {x: 8, y: 4}, {x: 8, y: 6}, {x: 8, y: 8}, {x: 7, y: 1}, {
-                    x: 7,
-                    y: 3
-                }, {x: 7, y: 5}, {x: 7, y: 7},
-                    {x: 6, y: 2}, {x: 6, y: 4}, {x: 6, y: 6}, {x: 6, y: 8}],
-                red_checkers_position: [{x: 1, y: 2}, {x: 1, y: 4}, {x: 1, y: 6}, {x: 1, y: 8}, {x: 2, y: 1}, {
-                    x: 2,
-                    y: 3
-                }, {x: 2, y: 5}, {x: 2, y: 7},
-                    {x: 3, y: 2}, {x: 3, y: 4}, {x: 3, y: 6}, {x: 3, y: 8}],
-                position_x: [1, 2, 3, 4, 5, 6, 7, 8],
-                position_y: [1, 2, 3, 4, 5, 6, 7, 8],
-                board: [
-                    [0, 1, 0, 2]
-                ]
-
-            }
-        }
+export default {
+  name: "Checkers",
+  data: () => {
+    return {
+      check: [0, 0],
+      boardKey: 0,
+      board: [
+        [0, 1, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0, 0]
+      ],
+      selectedCell: null
     }
+  },
+  methods: {
+    getCellColor: function (lineIndex, cellIndex) {
+      return ((lineIndex + 1)%2 == 0 && (cellIndex+1) %2 !=0) || ((lineIndex + 1)%2 != 0 && (cellIndex+1)% 2 ==0)
+    },
+    handleClickCell: function (lineIndex, cellIndex, cell) {
+      // console.log(lineIndex)
+      // console.log(cellIndex)
+      // console.log(cell)
+      console.log(this.selectedCell)
+      if (cell !== 0 && !this.selectedCell) {
+        this.selectedCell = {
+          lineIndex: lineIndex,
+          cellIndex: cellIndex,
+          cell: cell
+        }
+      } else if (this.selectedCell) {
+        this.board[lineIndex][cellIndex] = this.selectedCell.cell
+        this.board[this.selectedCell.lineIndex][this.selectedCell.cellIndex] = 0
+        this.selectedCell = null
+        ++this.boardKey
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-    .checkers {
-        display: flex;
-        justify-content: center;
-        flex-direction: column;
-        margin-bottom: 50px;
+.checkers {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 50px;
 
-        &__row {
-            display: flex;
-        }
+  &__row {
+    display: flex;
+  }
 
-        &__checker {
-            background-color: cornflowerblue;
-            border-radius: 50%;
-            width: 60px;
-            height: 60px;
-            margin: auto;
-            margin-top: 10px;
-            cursor: pointer;
+  &__checker {
+    background-color: cornflowerblue;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    margin: auto;
+    margin-top: 10px;
+    cursor: pointer;
 
-            &_red-color {
-                background-color: #DC143C;
-            }
-        }
-
-        &__title {
-            display: block;
-            font-size: 30px;
-        }
-
-        &__field {
-            display: flex;
-            flex-wrap: wrap;
-            width: 640px;
-            margin-left: auto;
-            margin-right: auto;
-            border: 2px solid black;
-            align-items: flex-start;
-
-        }
-
-        &__cell {
-            width: 80px;
-            height: 80px;
-            background-color: white;
-
-            &_black-color {
-                background-color: black;
-            }
-        }
+    &_red-color {
+      background-color: #DC143C;
     }
+  }
+
+  &__title {
+    display: block;
+    font-size: 30px;
+  }
+
+  &__field {
+    display: flex;
+    flex-wrap: wrap;
+    width: 640px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 2px solid black;
+    align-items: flex-start;
+
+  }
+
+  &__cell {
+    width: 80px;
+    height: 80px;
+    background-color: white;
+
+    &_black-color {
+      background-color: black;
+    }
+  }
+}
 </style>
